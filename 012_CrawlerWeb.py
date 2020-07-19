f8bd032a19db10daea070117312876052298be9a
kevin <monoserve174@gmail.com>
2020-07-19 16:51:33 +0800

    built web show data.

--- 012_CrawlerWeb.py
+++ 012_CrawlerWeb.py
@@ -1,11 +1,29 @@
 from flask import Flask, url_for, render_template
-
+import json
+import os
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    filepath = os.path.abspath(os.path.dirname(__file__))
+    filepath += '/static/beauty_data.json'
+    with open(file=filepath, mode='r', encoding='UTF-8') as f:
+        pic_data = json.loads(f.read())
+    for idx, pic in enumerate(pic_data):
+        pic['id'] = idx
+    return render_template('index.html', pic_data=pic_data)
+
+
+@app.route('/album/<int:id>')
+def album(id):
+    filepath = os.path.abspath(os.path.dirname(__file__))
+    filepath += '/static/beauty_data.json'
+    with open(file=filepath, mode='r', encoding='UTF-8') as f:
+        pic_data = json.loads(f.read())
+    for idx, pic in enumerate(pic_data):
+        pic['id'] = idx
+    return render_template('album.html', pic_data=pic_data[id])
 
 
 if __name__ == '__main__':
